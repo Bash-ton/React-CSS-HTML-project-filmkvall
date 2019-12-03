bae9e3c6c1e5bf8c3702cf588b58dcbd0626c88c
Federspiel <josef.federspiel@gmail.com>
2019-12-03 11:54:01 +0100

    added search by title

--- src/Data/apifetch.js
+++ src/Data/apifetch.js
@@ -1,11 +1,14 @@
 import {API_KEY} from "./apiConfig";
 import {ENDPOINT} from "./apiConfig";
 const BASE_URL = ENDPOINT;
-const httpOptions = {
-    headers: { "X-Mashape-Key": API_KEY}
-};
+
 class apifetch {
     constructor() {
+    }
+
+    getMovieByTitle(title){
+        let url = `${BASE_URL}3/search/movie/?language=en-US&page=1&include_adult=false&api_key=${API_KEY}&query=${title}`
+        return fetch(url).then((this.processResponse))
     }
 
     getTrending(){
